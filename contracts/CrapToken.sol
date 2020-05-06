52accde9d8fcaa1af7dc9e53eb28844c8103d212
dsajwan98 <37627908+dsajwan98@users.noreply.github.com>
2020-05-06 22:56:10 +0530

    Transferring Tokens

--- contracts/CrapToken.sol
+++ contracts/CrapToken.sol
@@ -10,11 +10,24 @@ contract CrapToken{
     //Variables
     uint256 public totalSupply;
     mapping(address=>uint256) public balanceOf;
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
     //Constructor
     constructor(uint256 _initialSupply) public{
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
-    //Set Tokens
-    //Read the number of Tokens
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        //Exception if account doesn't have enough token
+        require(balanceOf[msg.sender]>=_value,"Enough Token");
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender,_to,_value);
+        return true;
+    }
 }
--- test/CrapToken.js
+++ test/CrapToken.js
@@ -27,5 +27,30 @@ contract("CrapToken",function(accounts){
        }).then(function(adminBalance){
            assert.equal(adminBalance.toNumber(),1000000,"Sets the balance of Admin");
        });
-   }); 
+   });
+   
+   it("Transfers token ownership",function(){
+       return CrapToken.deployed().then(function(instance){
+           tokenInstance = instance;
+           return tokenInstance.transfer.call(accounts[1],999999999999999);
+       }).then(assert.fail).catch(function(error){
+           assert(error.message.indexOf('revert')>=0,'Error message must contain revert');
+           return tokenInstance.transfer.call(accounts[1],250000,{from:accounts[0]});
+       }).then(function(success){
+           assert.equal(success,true,"Transfer succeed");
+           return tokenInstance.transfer(accounts[1],250000,{from:accounts[0]});
+       }).then(function(receipt){
+           assert.equal(receipt.logs.length,1,'triggers one event');
+           assert.equal(receipt.logs[0].event,'Transfer','Should be the Transfer event');
+           assert.equal(receipt.logs[0].args._from,accounts[0],'Logs the account of sender');
+           assert.equal(receipt.logs[0].args._to,accounts[1],'Logs the account of receiver');
+           assert.equal(receipt.logs[0].args._value,250000,'Logs the transfer amount');
+           return tokenInstance.balanceOf(accounts[1]);
+       }).then(function(balance){
+           assert.equal(balance.toNumber(),250000,'Adds the amount to the receiving account');
+           return tokenInstance.balanceOf(accounts[0]);
+       }).then(function(balance){
+           assert.equal(balance.toNumber(),750000,'Balance of account after deduction');
+       });
+   });
 });
