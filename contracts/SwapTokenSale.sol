b312e4c50cd40be1f660760ed32e4cd3774500a9
dsajwan98 <37627908+dsajwan98@users.noreply.github.com>
2020-05-07 19:07:48 +0530

    Purchasing ERC20 Tokens

--- contracts/SwapTokenSale.sol
+++ contracts/SwapTokenSale.sol
@@ -6,10 +6,27 @@ contract SwapTokenSale{
     address admin;
     SwapToken public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokensSold;
+
+    event Sell(address _buyer, uint256 _amount);
 
     constructor(SwapToken _tokenContract, uint256 _tokenPrice) public{
         admin = msg.sender;
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
+    }
+
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x,"Valid Multiplication");
+    }
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        require(msg.value == multiply(_numberOfTokens, tokenPrice),"Value of Tokens");
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens,"Admin must possess more tokens than demanded");
+        require(tokenContract.transfer(msg.sender, _numberOfTokens),"Must transfer the requested tokens");
+
+        tokensSold += _numberOfTokens;
+
+        emit Sell(msg.sender, _numberOfTokens);
     }
 }
