4bd9cef7e69c2d5228a5998aa268ae893850f0d0
dsajwan98 <37627908+dsajwan98@users.noreply.github.com>
2020-05-08 17:36:02 +0530

    Purchasing ERC20 Tokens through client side

--- src/js/app.js
+++ src/js/app.js
@@ -39,13 +39,38 @@ App = {
         App.contracts.SwapToken.deployed().then(function(swapToken) {
           console.log("Swap Token Address:", swapToken.address);
         });
-
+        App.listenForEvents();
         return App.render();
       });
     })
   },
 
+
+  // Listen for events emitted from the contract
+  listenForEvents: function() {
+    App.contracts.SwapTokenSale.deployed().then(function(instance) {
+      instance.Sell({}, {
+        fromBlock: 0,
+        toBlock: 'latest',
+      }).watch(function(error, event) {
+        console.log("event triggered", event);
+        App.render();
+      })
+    })
+  },
+
   render: function() {
+    if (App.loading) {
+      return;
+    }
+    App.loading = true;
+
+    var loader  = $('#loader');
+    var content = $('#content');
+
+    loader.show();
+    content.hide();
+
     // Load account data
     web3.eth.getCoinbase(function(err, account) {
       if(err === null) {
@@ -54,7 +79,51 @@ App = {
       }
     })
 
+    // Load token sale contract
+    App.contracts.SwapTokenSale.deployed().then(function(instance) {
+      swapTokenSaleInstance = instance;
+      return swapTokenSaleInstance.tokenPrice();
+    }).then(function(tokenPrice) {
+      App.tokenPrice = tokenPrice;
+      $('.token-price').html(web3.fromWei(App.tokenPrice, "ether").toNumber());
+      return swapTokenSaleInstance.tokensSold();
+    }).then(function(tokensSold) {
+      App.tokensSold = tokensSold.toNumber();
+      $('.tokens-sold').html(App.tokensSold);
+      $('.tokens-available').html(App.tokensAvailable);
+
+      var progressPercent = (Math.ceil(App.tokensSold) / App.tokensAvailable) * 100;
+      $('#progress').css('width', progressPercent + '%');
+
+      // Load token contract
+      App.contracts.SwapToken.deployed().then(function(instance) {
+        swapTokenInstance = instance;
+        return swapTokenInstance.balanceOf(App.account);
+      }).then(function(balance) {
+        $('.swap-balance').html(balance.toNumber());
+        App.loading = false;
+        loader.hide();
+        content.show();
+      })
+    });
   },
+
+  buyTokens: function() {
+    $('#content').hide();
+    $('#loader').show();
+    var numberOfTokens = $('#numberOfTokens').val();
+    App.contracts.SwapTokenSale.deployed().then(function(instance) {
+      return instance.buyTokens(numberOfTokens, {
+        from: App.account,
+        value: numberOfTokens * App.tokenPrice,
+        gas: 500000 // Gas limit
+      });
+    }).then(function(result) {
+      console.log("Tokens bought...")
+      $('form').trigger('reset') // reset number of tokens in form
+      // Wait for Sell event
+    });
+  }
 
 }
 
