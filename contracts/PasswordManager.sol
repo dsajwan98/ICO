51c6cd6ec3f7c5162430c3c7ecd62ce46bdeb355
dsajwan98 <37627908+dsajwan98@users.noreply.github.com>
2020-06-24 02:55:08 +0530

    Credential Creation functionality updated

--- contracts/PasswordManager.sol
+++ contracts/PasswordManager.sol
@@ -31,21 +31,10 @@ contract PasswordManager {
         revealValue = _value;
     }
 
-  // function delegatedTransfer(address a, uint256 v) public{
-        //bool status;
-        //bytes memory result;
-        //(status, result) = address(tokenContract).delegatecall(abi.encodePacked(bytes4(keccak256("transfer(address,uint256)")), a, v));
-        //address(tokenContract).call(abi.encodeWithSignature(("transfer(address,uint256)"), a, v));
-   // }
-
     function createCredential(string memory _value) public {
-       // address payable receiver = address(uint160(0x6c749E8BEE9460e64250922D39A519E78A637627));
-        //address payable sender = address(uint160(0xE718022A857C1D3F1d5b3E7b17ff81AB6EaedF94));
         // Require a valid credential value
         require(bytes(_value).length > 0,"Must have some data");
         require(tokenContract.balanceOf(msg.sender) >= revealValue,"Admin must possess more tokens than demanded");
-        //delegatedTransfer(msg.sender,revealValue);
-        //address(tokenContract).call(abi.encodeWithSignature(("transfer(address,uint256)"), msg.sender, revealValue));
         require(tokenContract.transferToCreate(address(tokenContract),revealValue,msg.sender),"Must transfer the requested tokens");
         // Increment credential count
         credentialCount ++;
